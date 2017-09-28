d5daf14b1447fedc8dbf0038370c0ef66892a9fc
Your Name <you@example.com>
2017-09-28 20:09:37 +0530

    authentication error login required

---
+++ draft.py
@@ -0,0 +1,83 @@
+"""Send an email message from the user's account.
+"""
+
+import base64
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+import os
+
+from googleapiclient import errors
+
+import key
+service = key.service
+message_body = 'this is my first draft created using api'   
+
+
+
+def create_draft(service, user_id, message_body):
+  """Create and insert a draft email. Print the returned draft's message and id.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message_body: The body of the email message, including headers.
+
+  Returns:
+    Draft object, including draft id and message meta data.
+  """
+  try:
+    message = {'message': message_body}
+    draft = service.users().drafts().create(userId=user_id, body=message).execute()
+
+    print 'Draft id: %s\nDraft message: %s' % (draft['id'], draft['message'])
+
+    return draft
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+    return None
+
+
+def send_message(service, user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print 'Message Id: %s' % message['id']
+    return message
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+
+def create_message(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+create_draft(service, 'me' , message_body)  
---
+++ key.py
@@ -0,0 +1,15 @@
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file,client,tools
+
+scope = 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.compose https://www.googleapis.com/auth/gmail.compose https://mail.google.com/'
+client_secret = 'client_secret.json'
+http = Http()
+
+store = file.Storage('storage.json')
+credz = store.get()
+if not credz or credz.invalid:
+    flow = client.flow_from_clientsecrets(client_secret,scope)
+    credz = tools.run_flow(flow,store)
+    
+service = build('gmail','v1',http=http)
